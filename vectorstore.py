464d85876c9afcf336bba723d0cd52791a1a1b06
jayiuk <jayiuk987@gmail.com>
2025-07-06 18:06:20 +0900

    vectorstore

--- vectorstore.py
+++ vectorstore.py
@@ -1,16 +1,25 @@
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpneAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 key = os.getenv('OPENAI_API_KEY')
-embeddingmodel = OpneAIEmbeddings(model = "text-embedding-3-small")
+embeddingmodel = OpenAIEmbeddings(model = "text-embedding-3-small")
 
 def create_vdb(docs, dir_path):
     vecstore = Chroma.from_documents(
         collection_name = 'jd_store',
         documents = docs,
+        embedding = embeddingmodel,
         persist_directory = dir_path
     )
     return vecstore
+
+def load_vectorstore(dir_path):
+    persist_db = Chroma(
+        persist_directory = dir_path,
+        embedding_function = embeddingmodel,
+        collection_name = 'jd_store'
+    )
+    return persist_db
